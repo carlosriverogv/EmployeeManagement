d305bc760c3275269e97f7ac6bd1901c6fac4b29
Muyiam <liam.alejoasensi@gmail.com>
2024-05-30 23:10:32 +0200

    Uso de list en utils

--- EmployeeApp/app/src/main/java/edu/carlosliam/employeeapp/ui/detail/DetailTrabajoActivity.kt
+++ EmployeeApp/app/src/main/java/edu/carlosliam/employeeapp/ui/detail/DetailTrabajoActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import edu.carlosliam.employeeapp.R
 import edu.carlosliam.employeeapp.databinding.ActivityTrabajoDetailBinding
 import edu.carlosliam.employeeapp.model.Trabajo
+import edu.carlosliam.employeeapp.utils.trabajoList
 
 class DetailTrabajoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTrabajoDetailBinding
@@ -34,17 +35,17 @@ class DetailTrabajoActivity : AppCompatActivity() {
 
         if (intent.getIntExtra(TRABAJO_ID, 0) > 0) {
             // Se obtiene el episodio seleccionado de la lista de episodios.
-            val trabajo : Trabajo = trabajoList.find { it.id == intent.getIntExtra(TRABAJO_ID, 0) }
+            val trabajo = trabajoList.find { it.codTrab.equals(intent.getStringExtra(TRABAJO_ID)) }
 
-            binding.tvDescripcion.text = trabajo.descripcion
-            binding.tvCategoria.text = trabajo.categoria
-            binding.tvCodTrabajo.text = resources.getString(R.string.txt_codTrab, trabajo.codTrab)
+            binding.tvDescripcion.text = trabajo?.descripcion
+            binding.tvCategoria.text = trabajo?.categoria
+            binding.tvCodTrabajo.text = resources.getString(R.string.txt_codTrab, trabajo?.codTrab)
 
-            var formatHTML = getString(R.string.txt_fecha_inicio, trabajo.fecIni)
+            var formatHTML = getString(R.string.txt_fecha_inicio, trabajo?.fecIni)
             binding.tvFecIni.setText(Html.fromHtml(formatHTML, Html.FROM_HTML_MODE_LEGACY))
 
             formatHTML = getString(R.string.txt_prioridad_detalle,
-                when (trabajo.prioridad) {
+                when (trabajo?.prioridad) {
                     1 -> resources.getString(R.string.txt_prioridad_urgente)
                     2 -> resources.getString(R.string.txt_prioridad_alta)
                     3 -> resources.getString(R.string.txt_prioridad_media)
